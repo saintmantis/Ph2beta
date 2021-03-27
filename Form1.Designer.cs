7b6999275f445b2a14c02166da08a85c9943d237
saintmantis <46139022+saintmantis@users.noreply.github.com>
2021-03-27 14:24:19 +0300

    photoshop

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Ph2beta
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.White;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(288, 51);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(288, 598);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(56, 23);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(363, 598);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(56, 23);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(288, 580);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(14, 15);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "X";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(363, 580);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(14, 15);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Y";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1074, 787);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "X";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ph2beta
+{
+	public partial class Form1 : Form
+	{
+		protected Bitmap img1 = null; // окошко изменения
+		List<Point> point = new List<Point> { new Point(0, 512), new Point(512, 0) };
+		public Form1()
+		{
+			InitializeComponent();
+			img1 = (Bitmap)pictureBox1.Image;
+		}
+
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			point.Add(new Point(e.X, e.Y));
+			point.Sort((x, y) => x.X.CompareTo(y.X));
+
+			using (Graphics g = Graphics.FromImage(img1))
+			{
+				g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+				g.SmoothingMode = SmoothingMode.HighQuality;
+				g.Clear(Color.White);
+			}
+
+			pictureBox1.Refresh();
+			drawing();
+			//координаты в окошки
+			textBox1.Text = Convert.ToString(e.X);
+			textBox2.Text = Convert.ToString(e.Y);
+
+			//точка для красоты
+			Graphics gg = pictureBox1.CreateGraphics();
+			Pen pen = new Pen(Color.Black, 8f);
+			gg.DrawEllipse(pen, e.X - 3, e.Y - 3, 8, 8);
+		}
+
+		private void drawing()
+		{
+			using (Graphics g = Graphics.FromImage(img1))
+			{
+				g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+				g.SmoothingMode = SmoothingMode.HighQuality;
+				var p = Pens.Red.Clone() as Pen;
+				p.Width = 5;
+				for (int i = 0; i < point.Count - 1; i++)
+				{
+					PointF point1 = new PointF(point[i].X, point[i].Y);
+					PointF point2 = new PointF(point[i + 1].X, point[i + 1].Y);
+					g.DrawLine(p, point1, point2);
+				}
+			}
+			pictureBox1.Refresh();
+		}
+	}
+}
